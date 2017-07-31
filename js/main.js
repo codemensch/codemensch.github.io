8e704129bc6d91ab1c220ac7aae512c76154111c
Cody Fontes <codyfontes@gmail.com>
2017-07-31 15:42:29 -0700

    Updated Smooth Scroll, Fixed Indents

--- js/main.js
+++ js/main.js
@@ -1,81 +1,101 @@
 $(document).ready(function(){
     
 
-    //Hover Interactions
-    $('.overlay').on({
-      'touchstart': function () {
-        $(this).css('opacity', 0.75);
-      },
+  //Hover Interactions
+  $('.overlay').on({
+    'touchstart': function () {
+      $(this).css('opacity', 0.75);
+    },
+
+    'touchend': function () {
+      $(this).css('opacity', 0);
+    }
+  });
+
+  $('.project-container').on({
+    'touchstart': function () {
+      $(this).css('background-color', '#d8dfe5');
+    },
+
+    'touchend': function () {
+      $(this).css('background-color', '#c0c6cc');
+    }
+  });
+
+  $('a .fa-envelope-o, a .fa-linkedin, a .fa-github-alt').on({
+    'touchstart': function () {
+      $(this).css('color', '#fa8072');
+    },
+
+    'touchend': function () {
+      $(this).css('color', '#c0c6cc');
+    }
+  });
+
+  //Nav Show/Hide
+  $('#toggle').click(function(e){
+    //e.preventDefault();
+    $('nav').slideToggle();
+    $('.fa-chevron-up').toggleClass('toggleDown');
+  });
     
-      'touchend': function () {
-        $(this).css('opacity', 0);
-      }
-    });
-    
-    $('.project-container').on({
-      'touchstart': function () {
-        $(this).css('background-color', '#d8dfe5');
-      },
-    
-      'touchend': function () {
-        $(this).css('background-color', '#c0c6cc');
-      }
-    });
-    
-    $('a .fa-envelope-o, a .fa-linkedin, a .fa-github-alt').on({
-      'touchstart': function () {
-        $(this).css('color', '#fa8072');
-      },
-    
-      'touchend': function () {
-        $(this).css('color', '#c0c6cc');
-      }
-    });
-    
-    //Nav Show/Hide
-    $('#toggle').click(function(e){
-      //e.preventDefault();
-      $('nav').slideToggle();
-      $('.fa-chevron-up').toggleClass('toggleDown');
-    });
-    
-    //Porfolio Project Overlay Click Events
-    
-    $(".modal-fullscreen").on('show.bs.modal', function () {
-      setTimeout( function() {
-        $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
-          }, 0);
-    });
-    
-    $(".modal-fullscreen").on('hidden.bs.modal', function () {
+  //Porfolio Project Overlay Click Events
+  
+  $(".modal-fullscreen").on('show.bs.modal', function () {
+    setTimeout( function() {
       $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
-    });
+        }, 0);
+  });
+  
+  $(".modal-fullscreen").on('hidden.bs.modal', function () {
+    $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+  });
+  
+  /* Select Which Modal to View */
+  var currentProject = "";
+  var currentLabel = "";
+  var currentInfo = "";
+  
+  $('#social, #steadicam, .close, .close-btn').click(function(){
+    if ($(this).attr('id') == 'social'){
+      currentProject = $('#social-modal').attr('id');
+      currentLabel = $('#socialLabel').attr('id');
+      currentInfo = $('.socialInfo').attr('class');
+    }
+    else if ($(this).attr('id') == 'steadicam') {
+      currentProject = $('#steadicam-modal').attr('id');
+      currentLabel = $('#steadicamLabel').attr('id');
+      currentInfo = $('.steadicamInfo').attr('class');
+    }
     
-    /* Select Which Modal to View */
-    var currentProject = "";
-    var currentLabel = "";
-    var currentInfo = "";
-    
-    $('#social, #steadicam, .close, .close-btn').click(function(){
-      if ($(this).attr('id') == 'social'){
-        currentProject = $('#social-modal').attr('id');
-        currentLabel = $('#socialLabel').attr('id');
-        currentInfo = $('.socialInfo').attr('class');
-      }
-      else if ($(this).attr('id') == 'steadicam') {
-        currentProject = $('#steadicam-modal').attr('id');
-        currentLabel = $('#steadicamLabel').attr('id');
-        currentInfo = $('.steadicamInfo').attr('class');
-      }
-      
-      $('#' + currentLabel).toggle();
-      $('#' + currentProject).toggle();
-      $('.' + currentInfo).toggle();
-    });
+    $('#' + currentLabel).toggle();
+    $('#' + currentProject).toggle();
+    $('.' + currentInfo).toggle();
+  });
 
   //Smooth Scrolling from links 
   // Select all links with hashes
-  
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
   
   //Highlight Current Nav Button on Scroll
   var sections = $('.section')
